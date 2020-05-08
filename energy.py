f5518df583cd9f8be8cb460d728a68ecb9dd09b3
alejordano <52293809+alejordano@users.noreply.github.com>
2020-05-08 13:28:22 -0300

    Update and rename energy_3.py to energy.py

--- energy.py
+++ energy.py
@@ -8,7 +8,7 @@
 
 import sklearn.preprocessing
 from sklearn.decomposition import PCA
-from clusters import atratores, atratores_classificacao, dados_localizados, dados_cat, classificacao
+#from clusters import atratores, atratores_classificacao, dados_localizados, dados_cat, classificacao
 # from dados_xlsx import new_controle, new_tumor
 from scipy.spatial import distance
 import numpy as np
@@ -16,7 +16,7 @@ from neupy import algorithms
 
 # In[96]:
 dhnet = algorithms.DiscreteHopfieldNetwork(mode='sync')
-teste = np.array([atratores[1], atratores[0]])  #AQUI TEM QUE MUDAR NO SEGUNDO (1 é tratado, 2 é controle 2 e 0 é controle 1)
+teste = np.array([atratores[0], atratores[1], atratores[2]])  #AQUI TEM QUE MUDAR NO SEGUNDO (1 é tratado, 2 é controle 2 e 0 é controle 1)
 dhnet.train(teste)
 pca = PCA(n_components=2)
 
@@ -108,10 +108,10 @@ for p_n in range(len(atratores)):
 
 ax.view_init(elev=6, azim=-72)
 ax.plot_surface(X, Y, Z, alpha = 0.5)#, cmap='Reds')
-ax.set_xlabel('1st pc')
-ax.set_ylabel('2st pc')
-ax.set_zlabel('Energy')
-ax.legend(fontsize=20)
+ax.set_xlabel('')
+ax.set_ylabel('')
+ax.set_zlabel('')
+
 
 ax2 = fig.add_subplot(122)
 c = ax2.pcolormesh(X, Y, Z, cmap='RdBu', vmin=Z.min(), vmax=Z.max())
@@ -129,7 +129,7 @@ for p_n in range(len(atratores)):
                     s=100, c="red")
         else:
             ax2.scatter(pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y,
-                        s=100, c="yellow", marker='^')
+                        s=100, c="yellow", marker='s')
     elif p_n == 2:
         marc2 += 1
         if marc2 < 18:
@@ -137,7 +137,7 @@ for p_n in range(len(atratores)):
                     s=100, c="green")
         else:
             ax2.scatter(pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y,
-                        s=100, c="green", marker = '^')
+                        s=100, c="green", marker = 's')
     else:
         marc3 += 1
         if marc3 < 48:
@@ -145,7 +145,7 @@ for p_n in range(len(atratores)):
                     s=100, c="blue")
         else:
             ax2.scatter(pontos[p_n][0] + correct_co_x, pontos[p_n][1] + correct_co_y,
-                        s=100, c="blue", marker = '^')
+                        s=100, c="blue", marker = 's')
 #plt.imshow(Z, cmap='hot', interpolation='nearest')
 ax2.axis([X.min(), X.max(), Y.min(), Y.max()])
 fig.colorbar(c)
@@ -167,10 +167,10 @@ for p_n in range(len(dados_localizados)):
         marc1 += 1
         if marc1 < 75:
             ax.scatter(pontos[p_n][0], pontos[p_n][1], energy(pca.inverse_transform(pontos[p_n])), s=100,
-                   c="yellow", label="Treated")
+                   c="red", label="Treated")
         else:
             ax.scatter(pontos[p_n][0], pontos[p_n][1], energy(pca.inverse_transform(pontos[p_n])), s=100,
-                       c="yellow", label="", marker = '^')
+                       c="red", label="", marker = 's')
     elif classificacao[p_n] == 0:
         marc2 += 1
         if marc2 < 48:
@@ -178,7 +178,7 @@ for p_n in range(len(dados_localizados)):
                    c="green", label="Control 1")
         else:
             ax.scatter(pontos[p_n][0], pontos[p_n][1], energy(pca.inverse_transform(pontos[p_n])), s=100,
-                       c="green", label="", marker = '^')
+                       c="green", label="", marker = 's')
     else:
         marc3 += 1
         if marc3 < 18:
@@ -186,29 +186,29 @@ for p_n in range(len(dados_localizados)):
                    c="blue", label="Control 2")
         else:
             ax.scatter(pontos[p_n][0], pontos[p_n][1], energy(pca.inverse_transform(pontos[p_n])), s=100,
-                       c="blue", label="", marker = '^')
+                       c="blue", label="", marker = 's')
 
         
-pontos = pca.transform(atratores)
-for p_n in range(len(atratores)):
-    if p_n == 1:
-        ax.scatter(pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y, energy(pca.inverse_transform([[pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y]])), s=200, c="purple", label="Treated centroid")
-    elif p_n == 2:
-        ax.scatter(pontos[p_n][0] + correct_co_x, pontos[p_n][1] + correct_co_y, energy(
-            pca.inverse_transform([[pontos[p_n][0] + correct_co_x, pontos[p_n][1] + correct_co_y]])) + 1000, s=200,
-                   c="red", label="Control centroid 2")
-    else:
-        ax.scatter(pontos[p_n][0] + correct_co_x, pontos[p_n][1] +  correct_co_y, energy(pca.inverse_transform([[pontos[p_n][0] + correct_co_x, pontos[p_n][1] + correct_co_y]])), s=200, c="plum", label="Control centroid 1")
+#pontos = pca.transform(atratores)
+#for p_n in range(len(atratores)):
+ #   if p_n == 1:
+  #      ax.scatter(pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y, energy(pca.inverse_transform([[pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y]])), s=200, c="purple", label="Treated centroid")
+   # elif p_n == 2:
+    #    ax.scatter(pontos[p_n][0] + correct_co_x, pontos[p_n][1] + correct_co_y, energy(
+     #       pca.inverse_transform([[pontos[p_n][0] + correct_co_x, pontos[p_n][1] + correct_co_y]])) + 1000, s=200,
+      #             c="red", label="Control centroid 2")
+    #else:
+     #   ax.scatter(pontos[p_n][0] + correct_co_x, pontos[p_n][1] +  correct_co_y, energy(pca.inverse_transform([[pontos[p_n][0] + correct_co_x, pontos[p_n][1] + correct_co_y]])), s=200, c="plum", label="Control centroid 1")
 
 ax.view_init(elev=6, azim=-72)
 ax.plot_surface(X, Y, Z, alpha = 0.5)#, cmap='Reds')
-ax.set_xlabel('1st pc')
-ax.set_ylabel('2st pc')
-ax.set_zlabel('Energy')
+ax.set_xlabel('')
+ax.set_ylabel('')
+ax.set_zlabel('')
 from collections import OrderedDict
 handles, labels = ax.get_legend_handles_labels()
 by_label = OrderedDict(zip(labels, handles))
-ax.legend(by_label.values(), by_label.keys(), prop={'size': 16})
+ax.legend(by_label.values(), by_label.keys(), prop={'size': 18})
 
 ax2 = fig.add_subplot(122)
 c = ax2.pcolormesh(X, Y, Z, cmap='RdBu', vmin=Z.min(), vmax=Z.max())
@@ -223,38 +223,40 @@ for p_n in range(len(dados_localizados)):
     if classificacao[p_n] == 1:
         marc4 += 1
         if marc4 < 75:
-            ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="yellow")
+            ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="red")
         else:
-            ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="yellow", marker = '^')
+            ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="red", marker = 's')
     elif classificacao[p_n] == 0:
         marc5 += 1
         if marc5 < 48:
             ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="green")
         else:
-            ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="green", marker = '^')
+            ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="green", marker = 's')
     elif classificacao[p_n] == 2:
         marc6 += 1
         if marc6 < 18:
             ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="blue")
         else:
-            ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="blue", marker = '^')
+            ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="blue", marker = 's')
         
-pontos = pca.transform(atratores)
-for p_n in range(len(atratores)):
-    if p_n == 1:
-        ax2.scatter(pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y, 
-                    s=100, c="purple")
-    elif p_n == 0:
-        ax2.scatter(pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y,
-                    s=100, c="plum")
-    else:
-        ax2.scatter(pontos[p_n][0] + correct_co_x, pontos[p_n][1]+ correct_co_y, 
-                    s=100, c="red")
+#pontos = pca.transform(atratores)
+#for p_n in range(len(atratores)):
+ #   if p_n == 1:
+  #      ax2.scatter(pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y, 
+   #                 s=100, c="purple")
+    #elif p_n == 0:
+     #   ax2.scatter(pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y,
+         #           s=100, c="plum")
+    #else:
+     #   ax2.scatter(pontos[p_n][0] + correct_co_x, pontos[p_n][1]+ correct_co_y, 
+      #              s=100, c="red")
 #plt.imshow(Z, cmap='hot', interpolation='nearest')
 ax2.axis([X.min(), X.max(), Y.min(), Y.max()])
 fig.colorbar(c)
+ax.set_xlabel('', fontsize=18)
+ax.set_ylabel('', fontsize=18)
+ax.set_zlabel('', fontsize=18)
 
+fig.savefig('myimage.png', format='png', dpi=300)
 
-#fig.savefig('myimage.png', format='png', dpi=300)
-
-#plt.show()
+plt.show()
