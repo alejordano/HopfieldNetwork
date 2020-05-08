f7703e7da86ad9c99ac0df842df2e0c635839f0c
alejordano <52293809+alejordano@users.noreply.github.com>
2020-05-08 13:42:28 -0300

    Create simulação_trastuzumabe.py

---
+++ simulação_trastuzumabe.py
@@ -0,0 +1,262 @@
+genes = [] 
+for i in range(410):
+  if dados_localizados[74][i] != dados_localizados[122][i]:
+    genes.append(i)
+
+print(genes)
+len(genes)
+
+euc = []
+inter = []
+samples = []
+objeto = []
+objetivo = [] 
+objeto = np.array(dados_localizados[122])
+dist= distance.euclidean(objeto, dados_localizados[74])
+objetivo = np.array(dados_localizados[74])
+print(dist)
+
+if dist > 0: 
+  for x in genes:
+    objeto[x] = objetivo[x]
+    dist = distance.euclidean(objeto, objetivo)
+    #print(dist)
+    inter.append(x)
+    samples.append(np.array(objeto))
+    euc.append(dist)
+
+dist = distance.euclidean(objeto, dados_localizados[74])
+dist3 = distance.euclidean(objeto, objetivo)
+
+print(dist, dist3, len(inter), len(euc), euc)
+
+list(samples[1])
+samples2 = list(samples)
+samples3 = []
+samples3.append(list(samples2[1]))
+for x in range(len(samples2)):
+  if x%30 == 0:
+    samples3.append(list(samples2[x]))
+    
+ from neupy import algorithms
+
+dhnet = algorithms.DiscreteHopfieldNetwork(mode='sync')
+teste = np.array([atratores[0], atratores[1], atratores[2]])  #AQUI TEM QUE MUDAR NO SEGUNDO (1 é tratado, 2 é controle 2 e 0 é controle 1)
+dhnet.train(teste)
+pca = PCA(n_components=2)
+
+
+# In[119]:
+
+teste = np.array(list(dados_localizados) + list(atratores), dtype=float)
+pca.fit(teste)
+
+energias_teste = []
+
+for y in teste:
+    energias_teste.append(dhnet.energy(y)[0])
+
+# In[120]:
+
+
+print(pca.explained_variance_ratio_)  
+print(pca)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+# In[123]:
+def recognize(input_vector):
+    soma = 1000000000000000
+    print("New input vector")
+    m = 0
+    vector = np.array(input_vector)
+    for y in range(len(teste)):
+        soma2 = (abs(teste[y] - vector)).sum()
+        if soma2 <= soma: # and soma2 != 0:
+            print("{} {}".format(soma2, y))
+            soma = soma2
+            m = y
+    return m
+
+binarizar_inverso = np.vectorize(lambda x: 0 if x < 0.5 else 1)
+def energy(input_vector):
+    input_vector = binarizar_inverso(input_vector)
+    # input_vector = teste[recognize(input_vector)]
+    energy = dhnet.energy(input_vector)[0]
+    return energy
+
+
+x = y = np.arange(-8.001, 8.001, 0.5)
+X, Y = np.meshgrid(x, y)
+energies = map(energy, pca.inverse_transform(list(zip(np.ravel(X), np.ravel(Y)))))
+#for data in zip(x,y):
+    #energies.append(energy(pca.inverse_transform(data)))
+
+# In[124]:
+
+zs = np.array(list(energies))
+Z = zs.reshape(X.shape)
+# In[125]:
+
+#pca.transform(pacientes)
+
+# In[138]:
+
+fig = plt.figure(figsize=(20, 10))
+ax = fig.add_subplot(121, projection='3d')
+
+correct_ca_y = 0
+correct_ca_x = 0
+correct_co_y = 0
+correct_co_x = 0
+
+pontos = pca.transform(atratores)
+for p_n in range(len(atratores)):
+    if p_n == 1:
+        ax.scatter(pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y, energy(pca.inverse_transform([[pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y]])) , s=100, c="red", label="treated")
+    elif p_n == 2:
+        ax.scatter(pontos[p_n][0] + correct_co_x, pontos[p_n][1] + correct_co_y, energy(pca.inverse_transform([[pontos[p_n][0] + correct_co_x, pontos[p_n][1] + correct_co_y]])), s=100, c="blue", label="control 2")
+    else:
+        ax.scatter(pontos[p_n][0] + correct_co_x, pontos[p_n][1] + correct_co_y, energy(pca.inverse_transform([[pontos[p_n][0] + correct_co_x, pontos[p_n][1] + correct_co_y]])) , s=100, c="green", label="control 1")
+
+ax.view_init(elev=6, azim=-72)
+ax.plot_surface(X, Y, Z, alpha = 0.5)#, cmap='Reds')
+ax.set_xlabel('1st pc')
+ax.set_ylabel('2st pc')
+ax.set_zlabel('Energy')
+ax.legend(fontsize=18)
+
+ax2 = fig.add_subplot(122)
+c = ax2.pcolormesh(X, Y, Z, cmap='RdBu', vmin=Z.min(), vmax=Z.max())
+
+marc1 = 0
+marc2 = 0
+marc3 = 0
+
+pontos = pca.transform(atratores)
+for p_n in range(len(atratores)):
+    if p_n == 1:
+        marc1 +=1
+        if marc1 < 75:
+            ax2.scatter(pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y,
+                    s=100, c="red")
+        else:
+            ax2.scatter(pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y,
+                        s=100, c="yellow", marker='s')
+    elif p_n == 2:
+        marc2 += 1
+        if marc2 < 18:
+            ax2.scatter(pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y,
+                    s=100, c="green")
+        else:
+            ax2.scatter(pontos[p_n][0] + correct_ca_x, pontos[p_n][1] + correct_ca_y,
+                        s=100, c="green", marker = 's')
+    else:
+        marc3 += 1
+        if marc3 < 48:
+            ax2.scatter(pontos[p_n][0] + correct_co_x, pontos[p_n][1]+ correct_co_y,
+                    s=100, c="blue")
+        else:
+            ax2.scatter(pontos[p_n][0] + correct_co_x, pontos[p_n][1] + correct_co_y,
+                        s=100, c="blue", marker = 's')
+#plt.imshow(Z, cmap='hot', interpolation='nearest')
+ax2.axis([X.min(), X.max(), Y.min(), Y.max()])
+fig.colorbar(c)
+
+#plt.show()
+
+# In[146]:
+
+marc1 = 0
+marc2 = 0
+marc3 = 0
+
+fig = plt.figure(figsize=(20, 10))
+ax = fig.add_subplot(121, projection='3d')
+
+pontos = pca.transform(dados_localizados)
+for p_n in range(len(dados_localizados)):
+    if classificacao[p_n] == 1:
+        marc1 += 1
+        if marc1 < 75:
+            ax.scatter(pontos[p_n][0], pontos[p_n][1], energy(pca.inverse_transform(pontos[p_n])), s=100,
+                   c="red", label="Treated")
+        else:
+            ax.scatter(pontos[p_n][0], pontos[p_n][1], energy(pca.inverse_transform(pontos[p_n])), s=100,
+                       c="red", label="", marker = 's')
+    elif classificacao[p_n] == 0:
+        marc2 += 1
+        if marc2 < 48:
+            ax.scatter(pontos[p_n][0], pontos[p_n][1], energy(pca.inverse_transform(pontos[p_n])), s=100,
+                   c="green", label="Control 1")
+        else:
+            ax.scatter(pontos[p_n][0], pontos[p_n][1], energy(pca.inverse_transform(pontos[p_n])), s=100,
+                       c="green", label="", marker = 's')
+    elif classificacao[p_n] == 3:
+        marc3 += 1 
+        if marc3 < 143:
+            ax.scatter(pontos[p_n][0], pontos[p_n][1], energy(pca.inverse_transform(pontos[p_n])), s=100,
+                       c="black", label="Trajectory", marker = '<')
+            
+#for i, txt in enumerate(range(len(dados_localizados))):
+ # ax.annotate(txt, (pontos[i][0], pontos[i][1]))
+        
+ax.view_init(elev=6, azim=-72)
+ax.plot_surface(X, Y, Z, alpha = 0.5)#, cmap='Reds')
+ax.set_xlabel('1st pc')
+ax.set_ylabel('2st pc')
+ax.set_zlabel('Energy')
+from collections import OrderedDict
+handles, labels = ax.get_legend_handles_labels()
+by_label = OrderedDict(zip(labels, handles))
+ax.legend(by_label.values(), by_label.keys(), prop={'size': 18})
+
+ax2 = fig.add_subplot(122)
+c = ax2.pcolormesh(X, Y, Z, cmap='RdBu', vmin=Z.min(), vmax=Z.max())
+
+
+marc4 = 0
+marc5 = 0
+marc6 = 0
+
+pontos = pca.transform(dados_localizados)
+for p_n in range(len(dados_localizados)):
+    if classificacao[p_n] == 1:
+        marc4 += 1
+        if marc4 < 75:
+            ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="red")
+        else:
+            ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="red", marker = 's')
+    elif classificacao[p_n] == 0:
+        marc5 += 1
+        if marc5 < 48:
+            ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="green")
+        else:
+            ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="green", marker = 's')
+    elif classificacao[p_n] == 3:
+        marc6 += 1
+        if marc6 < 173:
+            ax2.scatter(pontos[p_n][0], pontos[p_n][1], s=100, c="black", marker = '<')
+
+#for i, txt in enumerate(range(len(dados_localizados))):
+ # ax2.annotate(txt, (pontos[i][0], pontos[i][1]))
+
+ax2.axis([X.min(), X.max(), Y.min(), Y.max()])
+ax.set_xlabel('', fontsize=18)
+ax.set_ylabel('', fontsize=18)
+ax.set_zlabel('', fontsize=18)
+
+print(
+        "Explained variance 1st cp: {}; \
+    2cp:{}".format(pca.explained_variance_ratio_[0],
+                  pca.explained_variance_ratio_[1]))
+
+
+fig.colorbar(c)
+
+
+
+plt.show()
